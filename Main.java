c4d231ee5d21d53a889429c3c3a67984ae0814b7
NaziMikhelashvili <154763903+NaziMikhelashvili@users.noreply.github.com>
2024-06-04 00:24:03 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,31 @@
+package itog;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Tel mytel = new Tel();
+        mytel.add("MATE", 12344);
+        mytel.add("MITE", 56677);
+        mytel.add("MOTE", 1122);
+        mytel.add("MITA", 555555);
+        mytel.add("MOTA", 334535);
+        mytel.add("MATE", 65655);
+        mytel.add("MTQ", 5555555);
+        mytel.add("MTA", 6564);
+        mytel.add("MTQ", 464);
+        mytel.add("MOTA",4554555);
+        mytel.add("RESA", 111134);
+        mytel.add("TUR", 22225);
+        mytel.add("MITA", 989899);
+        mytel.add("MITE", 5555);
+        
+        // mytel.find("MATE");
+        // mytel.removePhone("MOTA", 45);
+        // mytel.find("MITA");
+        
+        mytel.showAll();
+     
+
+
+    }
+}
